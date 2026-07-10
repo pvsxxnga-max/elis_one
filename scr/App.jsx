f7c9c1625ef43ddf1735b812512d1902821e5549
pvsxxnga-max <pvsxxnga@gmail.com>
2026-07-10 20:49:55 +0700

    Update App.jsx

--- scr/App.jsx
+++ scr/App.jsx
@@ -1,1 +1,12 @@
-.
+import React from 'react';
+import Login from './pages/Login'; // ตรวจสอบว่าสร้างไฟล์ Login.jsx ในโฟลเดอร์ pages แล้ว
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
